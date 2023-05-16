f242d3dcf010a6ed1a1fd67ceb7f1188fe174b21
Sidney Shafer <sidney@mnshafers.com>
2023-05-16 06:24:45 -0500

    intro to Object Oriented Programming

---
+++ 13-OOP - Constructors and Prototypes/01-What is OOP/script.js
@@ -0,0 +1,55 @@
+// WHAT IS OOP?
+/**
+ * OOP/Object Oriented Programming is a programming paradigm or a way of thinking about and structuring code.
+ * It is a specific style to implement certain principles.
+ * There are other paradigms such as procedural, functional and logic.
+ * OOP is extremely common in many different languages.
+ */
+
+// WHAT IS AN OBJECT?
+/**
+ * 'Self-contained' piece of code that is a collection of methods and properties.
+ * Objects are used a building blocks and can interact with one another.
+ * There are many ways to create objects in JS.
+ * We have object literals, constructor functions, classes, factories and more.
+ * We have worked with many objects already.
+ * We have even created some using the object literal syntax.
+ */
+
+// EXAMPLES OF COMMON OBJECTS
+// User, Post, Comment, UI Component, Product, Order, Cart, Customer, Employee, Message, Book, Item
+
+// WHY USE OOP
+/**
+ * Can make your code easier to read and understand.
+ * Prevents spaghetti code.
+ * Flexible and allows you to build complex applications.
+ * NOT always needed. OOP can be overkill ofr smaller projects. 
+ * It's greate for more complex projects, expecially if working on a team.
+ */
+
+// COMPONENTS OF OOP
+/**
+ * Constructor Functions (JS): creates a blueprint for an object
+ * Prototypes (JS): a way for objects to inherit properties and methods from one another.
+ * Classes: another way to create a blueprint. "Syntatic Sugar" in JS, but part of many other languages.
+ * Instances: Objects are "instantiated" using the blueprint (Constructor or Class)
+ */
+
+// 4 PRINCIPLES OF OOP
+/**
+ * #1 ABSTRACTION
+ * - Hiding all but the relevent parts of an object in order to reduce complexity and increase efficiency.
+ * - For example, when we use fetch() or we create an event listener with addEventListener(), we have no clue how it works. We only know about the methods and parts that are relevent to what we're doing.
+ * 
+ * #2 ENCAPSULATION
+ * - The process of wrapping up data and methods into a unit such as a class or function.
+ * - Encapsulation usually includes some kind of "data hiding".
+ * 
+ * #3 INHERITANCE
+ * - The process of inheriting the properties and methods from a parent class or constructor.
+ * - A way to reuse code and reduce redundancy.
+ * 
+ * #4 POLYMORPHISM - 'poly' menas many, 'morph' means form
+ * - The process of inheriting the properties and methods from a parent class or constructor.
+ */

1b2a200016d038aeb6539794490d9397c2f80754
Sidney Shafer <sidney@mnshafers.com>
2023-05-16 06:48:56 -0500

    constructor functions

---
+++ 13-OOP - Constructors and Prototypes/03-Constructor Functions/script.js
@@ -0,0 +1,33 @@
+// When the 'new' keyword is used:
+/**
+ * 1. A new empty object is created.
+ * 2. The constructor function is called with the arguments that we passed in.
+ * 3. The 'this' keyword is set to the new empty object.
+ * 4. The new object is returned from the constructor function.
+ */
+
+// Date() is a constructor function - whenever you see the 'new' keyword
+//const now = new Date();
+
+function Rectangle(name, width, height) {
+    this.name = name;
+    this.width = width;
+    this.height = height;
+    this.area = function () {
+        return this.height * this.width;
+    }
+}
+
+const rect1 = new Rectangle('Rectangle 1', 10, 10);
+
+console.log(rect1);
+console.log(rect1.area());
+
+const rect2 = new Rectangle('Rectangle 2', 20, 10);
+const rect3 = new Rectangle('Rectangle 3', 5, 8);
+
+console.log(rect2.name, rect3.name);
+console.log(rect2.area(), rect3.area());
+
+console.log(rect1.constructor);
+console.log(rect2 instanceof Rectangle);

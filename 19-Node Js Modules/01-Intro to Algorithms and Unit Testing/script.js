6f75333ef499d44208701175b0233d9a8164bbfd
Sidney Shafer <sidney@mnshafers.com>
2025-01-23 16:41:23 -0600

    Delete 19-Node Js Modules directory

--- 19-Node Js Modules/01-Intro to Algorithms and Unit Testing/script.js
+++
@@ -1,9 +0,0 @@
-// Node.js Modules Covered:
-/**
- * fs - work with file system. Manage files and folders
- * path - helpers for working with file paths
- * os - operating system information
- * url - parse and format URLs
- * querystring - helper for working with query strings from URLs
- * http - create server and listen for HTTP requests
- */

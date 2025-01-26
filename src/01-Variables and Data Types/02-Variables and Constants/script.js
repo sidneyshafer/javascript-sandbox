d32562c1ab85f2c28ad063a6c962f0bea731851e
Sidney Shafer <sidney@mnshafers.com>
2025-01-26 10:14:53 -0600

    Update code

--- src/01-Variables and Data Types/02-Variables and Constants/script.js
+++ src/01-Variables and Data Types/02-Variables and Constants/script.js
@@ -1,15 +1,60 @@
-// Ways to declare a variable
-// var, let, const
-
+// Variable Declaration
+// Three main ways to declare variables: var, let, and const.
 let firstName = 'Sidney';
 let lastName = 'Shafer';
+console.log(firstName, lastName); // Output: Sidney Shafer
 
-console.log(firstName, lastName)
+// Re-assigning Variables
+var temp = 'temp string';
+temp = 'new string';
 
 let age = 30;
-console.log(age);
+age = 31;
+// console.log(age);
 
-// Naming Conventions
+let score;
+score = 1;
+console.log(score); // Output: 1
+
+// Scope Behavior
+if (true) {
+    score += 1;
+}
+console.log(score); // Output: 2
+
+// Differences between Let and Var
+if (true) {
+    let x = 10; // x is only accessible within this "if" block
+    var y = 20; // y is accessible outside the "if" block as well
+}
+// console.log(x); // This will throw an error because "x" is not accessible here
+console.log(y); // This will print 20
+
+// Constants
+const x = 100;
+// x = 200; // Error: Cannot be directly re-assigned
+// const score1; // Error: Cannot declare without initializing it
+
+const array = [1, 2, 3, 4];
+array.push(5);
+console.log(array); // Outputs: [1, 2, 3, 4, 5]
+
+const person = {
+    firstName: 'Sidney',
+    lastName: 'Shafer',
+    age: 19
+};
+
+person.firstName = 'Jane';
+person.lastName = 'Doe';
+person.email = 'example@gmail.com';
+console.log(person);
+
+// Declaring Multiple Variables
+let a, b, c; // Declares but does not initialize variables
+const d = 10, e = 20, f = 30; // Declares and initializes constants
+
+// Variable Naming Conventions
 // -- Only letters, numbers, underscores, and dollar signs
 // -- Can't start with a number
 
@@ -18,38 +63,3 @@ console.log(age);
 // first_name = underscore
 // FirstName = PascalCase
 // firstname = lowercase
-
-// Re-assigning Variables
-age = 31;
-console.log(age);
-
-let score;
-score = 1;
-console.log(score);
-
-if (true) {
-    score += 1;
-}
-console.log(score);
-
-const x = 100;
-// x = 200; -- const cannot be directly re-assigned
-// const score1; -- cannot declare const without initializing it
-
-const array = [1,2,3,4];
-array.push(5);
-console.log(array);
-
-const person = {
-    firstName: 'Sidney',
-    lastName: 'Shafer',
-    age: 19
-};
-
-person.firstName = 'Allie';
-person.email = 'allie@gmail.com';
-console.log(person);
-
-// Declare multiple values at once
-let a, b, c;
-const d = 10, e = 20, f = 30;
